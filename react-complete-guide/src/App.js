6139b230a373d482ad6c676b32e0d87a7b106ed2
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-11 12:27:19 +0700

    Finishing flexible component solution

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -25,14 +25,24 @@ class App extends Component {
     })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28},
-        { name: event.target.value, age: 29},
-        { name: 'Stephanie', age: 29}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // This is the modern approach, equivalent would be:
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person
+
+    this.setState({persons: persons})
   }
 
   deletePersonHandler = (personIndex) => {
@@ -61,16 +71,14 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-            {this.state.persons.map((person, index) => {
-              return (
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  key={person.id}
-                />
-              )
-            })}
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
         </div>
       );
     }
