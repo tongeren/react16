96fe1cc4987032e74fb42ad9f21b9614bcbcb252
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-17 15:51:55 +0700

    Adapting Char component

--- assignments/list-conditionals/src/Char/Char.js
+++ assignments/list-conditionals/src/Char/Char.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Char.css';
 
 const char= (props) => {
   return (
-    <div className="CharComponent">
-
+    <div className="Char" onClick={props.clicked}>
+      {props.character}
     </div>
-  )
+  );
 };
 
 export default char;
