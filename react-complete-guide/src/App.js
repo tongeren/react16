d0ec78219bc15b0dd55204d5eb0e5f61ffe08c91
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-18 14:32:01 +0700

    Finishing error boundaries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -65,12 +66,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            return (
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              </ ErrorBoundary>)
           })}
         </div>
       );
