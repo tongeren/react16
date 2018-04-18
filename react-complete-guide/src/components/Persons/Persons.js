dbd262583f8aacdda00e004fb8711563b3d5fdb3
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-18 17:17:02 +0700

    Making classes

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-            return (
-                <Person
-                  click={() => props.clicked(index)}
+class Persons extends Component {
+  render () {
+    return this.props.persons.map((person, index) => {
+            return <Person
+                  click={() => this.props.clicked(index)}
                   name={person.name}
                   age={person.age}
                   key={person.id}
-                  changed={(event) => props.changed(event, person.id)}/>
-                )
-          }
-);
+                  changed={(event) => this.props.changed(event, person.id)}/>
 
-export default persons;
+          });
+   }
+}
+
+export default Persons;
