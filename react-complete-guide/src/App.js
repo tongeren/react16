c5fcda214d2a1a02068e6a0c91d982b80711ed7e
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-18 12:23:55 +0700

    Committing before ejecting

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot }  from 'radium';
 import './App.css';
 import Person from './Person/Person'
 
@@ -65,11 +64,7 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -88,10 +83,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -103,20 +94,18 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App </h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App </h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button
+           style={style}
+          onClick={this.togglePersonsHandler}>Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
-export default Radium(App);
+export default App;
