976973fdccb2de6e00628bca4a517ed0c4aee105
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-22 14:28:24 +0700

    Using class factory method

--- react-complete-guide/src/hoc/WithClass.js
+++ react-complete-guide/src/hoc/WithClass.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // const withClass = (props) => (
 //   <div className={props.classes}>
@@ -6,12 +6,24 @@ import React from 'react';
 //   </div>
 // );
 
-const withClass = (WrappedComponent, className) => {
-  return (props) => (
-    <div className={className}>
-      <WrappedComponent {...props}/>
-    </div>
-  )
+// const withClass = (WrappedComponent, className) => {
+//   return (props) => (
+//     <div className={className}>
+//       <WrappedComponent {...props}/>
+//     </div>
+//   )
+// }
+
+const withClass = (WrappedComponent, className) => { // As a class factory
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props}/>
+        </div>
+      )
+    }
+  }
 }
 
 export default withClass;
