548615a6dbc568bbd4596154f0ccba1fd5c1d20f
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-25 16:52:57 +0700

    Adding a toolbar

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,7 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continu to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
   <Fragment>
-    <div> Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
