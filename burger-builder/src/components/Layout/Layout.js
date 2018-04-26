e241f7040ff1ebd30d1d4940187fb132e2553c16
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-26 15:42:14 +0700

    Adding a responsive toolbar

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Fragment>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>
   </Fragment>
-);
+)
 
 export default layout;
