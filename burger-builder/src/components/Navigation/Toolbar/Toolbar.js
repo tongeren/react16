7adf34b52757b48368a799bd7809214d5b73fa04
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-26 16:07:12 +0700

    Adjusting logo size (second method)

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo height="80%" />
+    <div className={classes.Logo}>
+        <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
