abd7eac16731144d1aa517ae81ffdd5162d0c6bc
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-29 23:16:36 +0700

    Preparing for routing

--- http-ajax/src/App.js
+++ http-ajax/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom ';
+import { BrowserRouter } from 'react-router-dom';
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-    render() {
+    render () {
         return (
             <BrowserRouter>
                 <div className="App">
