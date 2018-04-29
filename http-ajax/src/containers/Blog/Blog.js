24be302740e680b803c1f72d5e793332228d5fb2
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-29 23:25:22 +0700

    Rendering components for routes

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import './Blog.css';
 
@@ -14,7 +15,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
