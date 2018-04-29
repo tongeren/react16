418973abddac70d59479af5d56dc16be2ea4acb7
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-29 09:28:49 +0700

    Sensing a POST request to firebase

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENTS_PRICES = {
   salad: 0.5,
@@ -81,7 +82,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+        ingredients: this.state.ingredients,
+        price: this.state.totalPrice,
+        customer: {
+            name: 'Duncan van Tongeren',
+            address: {
+              street: 'Testreet 1',
+              zipCode: '2355624',
+              country: 'Germany'
+            },
+            email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
+    }
+    //<node_name>.json only for firebase, otherwise only <node_name>
+    axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
   }
 
   render () {
