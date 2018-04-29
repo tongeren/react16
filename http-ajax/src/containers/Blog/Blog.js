7b0a334b0f0bd0c7ba962407a5391a7b02304c54
Duncan van Tongeren <yacayogo@gmail.com>
2018-04-29 23:28:31 +0700

    Switching between pages

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -16,6 +17,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
