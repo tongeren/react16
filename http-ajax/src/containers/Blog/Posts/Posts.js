2ef373b2d57a28b7cac1c8db9d2d72a6197fee48
Duncan van Tongeren <yacayogo@gmail.com>
2018-05-03 14:36:43 +0700

    Navigating programmatically

--- http-ajax/src/containers/Blog/Posts/Posts.js
+++ http-ajax/src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import axios from '../../../axios';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -30,7 +30,9 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-      this.setState({selectedPostId: id});
+      // this.setState({selectedPostId: id});
+      // this.props.history.push({pathname: '/' + id});
+      this.props.history.push('/' + id);
     }
 
     render () {
@@ -38,12 +40,14 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
             return (
-                <Link to={'/'+post.id} key={post.id}>
+                // <Link to={'/'+post.id} key={post.id}>
                     <Post
+                        key={post.id}
                         title={post.title}
                         author={post.author}
                         clicked={() => this.postSelectedHandler(post.id)} />
-                </Link> );
+                //</Link>
+                );
             });
         }
         return (
