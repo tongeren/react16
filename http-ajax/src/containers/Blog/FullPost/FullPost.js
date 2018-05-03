35aaf3e6f460d533db4a568238b300d834135a2f
Duncan van Tongeren <yacayogo@gmail.com>
2018-05-03 13:52:32 +0700

    Extracting route parameters

--- http-ajax/src/containers/Blog/FullPost/FullPost.js
+++ http-ajax/src/containers/Blog/FullPost/FullPost.js
@@ -8,10 +8,11 @@ class FullPost extends Component {
       loadedPost: null
     }
 
-    componentDidUpdate () {
-        if (this.props.id) {
+    componentDidMount () {
+      console.log(this.props);
+        if (this.props.match.params.id) {
             if (!this.state.loadedPost || (this.state.loadedPost.id !== this.props.id)) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.match.params.id)
                     .then(response => {
                         // console.log(response);
                         this.setState({loadedPost: response.data});
