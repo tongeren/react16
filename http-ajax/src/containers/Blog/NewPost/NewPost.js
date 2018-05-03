ab8861f144258371500ef6ab25e78e53ca649659
Duncan van Tongeren <yacayogo@gmail.com>
2018-05-03 15:39:03 +0700

    Conditionally redirect

--- http-ajax/src/containers/Blog/NewPost/NewPost.js
+++ http-ajax/src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import './NewPost.css';
 
@@ -6,7 +7,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     componentDidMount () {
@@ -23,12 +25,18 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
+                this.setState({submitted: true});
             });
     }
 
     render () {
+        let redirect = null;
+        if (this.state.submitted) {
+          redirect = <Redirect to="/posts" />;
+        }
         return (
             <div className="NewPost">
+                {redirect}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
