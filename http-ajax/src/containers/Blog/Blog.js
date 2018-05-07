d90f6919fd130cbe9592775d3458bbc5e0972eee
Duncan van Tongeren <yacayogo@gmail.com>
2018-05-07 12:25:19 +0700

    Using guards

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -6,6 +6,10 @@ import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -29,7 +33,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 {/* <Route path="/" component={Posts} /> */}
--- http-ajax/src/containers/Blog/NewPost/NewPost.js
+++ http-ajax/src/containers/Blog/NewPost/NewPost.js
@@ -12,6 +12,7 @@ class NewPost extends Component {
     }
 
     componentDidMount () {
+      // If unauth => this.props.history.replace('/posts');
       console.log(this.props);
     }
 
