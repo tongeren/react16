f1a80c2b123d0da594d4ebac388385f8c55b22ce
Duncan van Tongeren <yacayogo@gmail.com>
2018-05-07 12:29:52 +0700

    Handling unknown routes

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -35,7 +35,8 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
                     <Route path="/posts" component={Posts} />
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not found</h1>} />
+                {/* <Redirect from="/" to="/posts" /> */}
                 {/* <Route path="/" component={Posts} /> */}
                 {/* <Route path="/:id" exact component={FullPost} /> */}
                 </Switch>
